91d5f1f5653f67f3d99d0d08ce7fbec0f7b84b43
cheptegei-create <davidchepte@gmail.com>
2020-04-10 15:09:20 +0300

    added attributes in the main.js file

--- js/main.js
+++ js/main.js
@@ -9,4 +9,24 @@ $(document).ready(function() {
         $(".design-img").toggle(1200);
         $(".design-img").show(1200);
     });
+    $(".dev-img").click(function() {
+        $(".dev-img").hide(1200);
+        $("#development").toggle(1200);
+        $("#development").show(1200);
+    })
+    $("#development").click(function() {
+        $("#development").hide(1200);
+        $(".dev-img").toggle(1200);
+        $(".dev-img").show(1200);
+    });
+    $(".product-img").click(function() {
+        $(".product-img").hide(1200);
+        $("#product").toggle(1200);
+        $("#product").show(1200);
+    })
+    $("#product").click(function() {
+        $("#product").hide(1200);
+        $(".product-img").toggle(1200);
+        $(".product-img").show(1200);
+    });
 });
